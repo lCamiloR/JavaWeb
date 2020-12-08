16314b7ea617209be9220bcbd94f4095b7fde733
Rai Benjamin da Silva <rai.benjamindasilva@gmail.com>
2020-12-08 19:28:35 -0300

    Correção dos validadores

--- CrudWebLivraria/src/crudweblivraria/negocio/ValidadorCpf.java
+++ CrudWebLivraria/src/crudweblivraria/negocio/ValidadorCpf.java
@@ -1,5 +1,7 @@
 package crudweblivraria.negocio;
 
+import java.util.InputMismatchException;
+
 import crudweblivraria.interfaces.IValidador;
 import crudweblivraria.model.domain.EntidadeDominio;
 import crudweblivraria.model.domain.Funcionario;
@@ -9,39 +11,63 @@ public class ValidadorCpf implements IValidador {
 	@Override
 	public String validarCampo(EntidadeDominio ent) {
 		
-		Funcionario f = (Funcionario) ent;
-		String campo = f.getCpf();
+		Funcionario funcionario = (Funcionario) ent;
+		String CPF = funcionario.getCpf();
 		
-		if (campo == null) {
-	        return "CPF não pode ser nulo.\n";
-	    }
-		if(campo.length()!=11) {
-			return "CPF tem de ter 11 caracteres.\n";
-		}
 		
-		try {
-			@SuppressWarnings("unused")
-			int i = Integer.parseInt(campo);
-		}catch(NumberFormatException nfe) {
-			return "CPF tem de ser númerico.\n";
-		}
-		return algoritmoCpf(campo);
-	}
-	
-	public String algoritmoCpf(String cpf) {
-		
-		int soma = 0;
-		
-		for(int i = 0; i<cpf.length(); i++) {
-			soma += Character.getNumericValue(cpf.charAt(i));
-		}	
-		
-		String strSoma = soma + "";
-		
-		if(strSoma.charAt(0) == strSoma.charAt(1)) return "";
-		
-		return "Não é um CPF válido.\n";
-	}
+		if (CPF.equals("00000000000") ||
+	            CPF.equals("11111111111") ||
+	            CPF.equals("22222222222") || CPF.equals("33333333333") ||
+	            CPF.equals("44444444444") || CPF.equals("55555555555") ||
+	            CPF.equals("66666666666") || CPF.equals("77777777777") ||
+	            CPF.equals("88888888888") || CPF.equals("99999999999") ||
+	            (CPF.length() != 11))
+	            return "CPF inválido.\n";
+
+	        char dig10, dig11;
+	        int sm, i, r, num, peso;
+
+	        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+	        try {
+	        // Calculo do 1o. Digito Verificador
+	            sm = 0;
+	            peso = 10;
+	            for (i=0; i<9; i++) {
+	        // converte o i-esimo caractere do CPF em um numero:
+	        // por exemplo, transforma o caractere '0' no inteiro 0
+	        // (48 eh a posicao de '0' na tabela ASCII)
+	            num = (int)(CPF.charAt(i) - 48);
+	            sm = sm + (num * peso);
+	            peso = peso - 1;
+	            }
+
+	            r = 11 - (sm % 11);
+	            if ((r == 10) || (r == 11))
+	                dig10 = '0';
+	            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+
+	        // Calculo do 2o. Digito Verificador
+	            sm = 0;
+	            peso = 11;
+	            for(i=0; i<10; i++) {
+	            num = (int)(CPF.charAt(i) - 48);
+	            sm = sm + (num * peso);
+	            peso = peso - 1;
+	            }
+
+	            r = 11 - (sm % 11);
+	            if ((r == 10) || (r == 11))
+	                 dig11 = '0';
+	            else dig11 = (char)(r + 48);
+
+	        // Verifica se os digitos calculados conferem com os digitos informados.
+	            if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
+	                 return "";
+	            else return "CPF inválido.\n";
+	                } catch (InputMismatchException erro) {
+	                return "CPF inválido.\\n";
+	            }
+	        }
 
 	
 }
